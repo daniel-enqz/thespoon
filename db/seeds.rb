f71e30b5dd98ff73b6830bd1b48ac0deee64df28
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-01 09:29:05 -0600

    updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+50.times do
+  Restaurant.create!(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    rating: rand(1..5)
+  )
+end
